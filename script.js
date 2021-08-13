e6e45c65be785c39854ddba296284497c89b6e12
Rusty <imkhazix@gmail.com>
2021-08-13 19:32:31 +0530

    Improved CSS

--- script.js
+++ script.js
@@ -5,48 +5,51 @@ const todoList = document.querySelector('.todo-list');
 
 
 // event listeners
-
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', checkDelete);
 
+document.getElementById("todo-input").addEventListener("keydown", function(e) {
+    if (e.keyCode == 13) { addTodo(); }
+}, false);
+
 // functions
-
 function addTodo(event) {
-    event.preventDefault(); 
     // prevents submission
-
-    const todoDiv = document.createElement('div');
-    todoDiv.classList.add('todo');
-
     // Create new items
-    const newTodo = document.createElement('li');
-    newTodo.innerText = todoInput.value;
-    newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo);
+    if(todoInput.value != "") {
+        const newTodo = document.createElement('li');
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add('todo');
+        newTodo.innerText = todoInput.value;
+        newTodo.classList.add('todo-item');
+        todoDiv.appendChild(newTodo);
 
-    // Mark finished items
-    const completedButton = document.createElement('button');
-    completedButton.innerHTML = `<i class="bi bi-check2-square"></i>`
-    completedButton.classList.add("complete-btn")
-    todoDiv.appendChild(completedButton);
-    // Delete item
-    const deleteButton = document.createElement('button');
-    deleteButton.innerHTML = `<i class="bi bi-trash-fill"></i>`
-    deleteButton.classList.add("trash-btn")
-    todoDiv.appendChild(deleteButton);
+        // Mark finished items
+        const completedButton = document.createElement('button');
+        completedButton.innerHTML = `<i class="bi bi-check2-square"></i>`
+        completedButton.classList.add("complete-btn")
+        todoDiv.appendChild(completedButton);
 
-    todoList.appendChild(todoDiv);
+        // Delete item
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = `<i class="bi bi-trash-fill"></i>`
+        deleteButton.classList.add("trash-btn")
+        todoDiv.appendChild(deleteButton);
 
-    // Clear textbox value after adding
-    todoInput.value = "";
+        todoList.appendChild(todoDiv);
+
+        // Clear textbox value after adding
+        todoInput.value = "";
+    }
+    else {
+        console.log("Enter something bro");
+    }
 
 }
-
 
 function checkDelete(e) {
     const item = e.target;
     console.log(item);
-
     // Delete item
     if(item.classList[0] === 'trash-btn') {
         const todoElement = item.parentElement;
@@ -58,7 +61,7 @@ function checkDelete(e) {
     }
     // Scratch it
     if(item.classList[0] === 'complete-btn') {
-        const todoElement = item.parentElement;
+        const todoElement = item.parentElement.firstChild;
         todoElement.classList.toggle("completed");
     }
 
