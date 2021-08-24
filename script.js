8a7643c0acf9b205962620dccc684235932eb055
Rusty <imkhazix@gmail.com>
2021-08-24 17:54:06 +0530

    Update
    Added clock, quotes, counter, stopwatch, color generator and todo list

--- script.js
+++ script.js
@@ -1,3 +1,201 @@
+const timeHeader = document.getElementById("time");
+const timeWords = document.getElementById("timeWords");
+var ones = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+];
+var tens = [
+    "twenty",
+    "thirty",
+    "fourty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+];
+
+function hourToWord(hour) {
+    if (hour > 12) {
+        hour -= 12;
+    }
+    return ones[hour];
+}
+
+function minutesToWords(minutes) {
+    let min = Number(minutes);
+    if (min > 0 && min < 10) {
+        return " o " + ones[min];
+    } else if (min >= 10 && min < 20) {
+        return " " + ones[min];
+    } else if (min % 10 == 0) {
+        return " " + tens[Math.floor(min / 10) - 2];
+    } else {
+        return (
+            " " +
+            tens[Math.floor(min / 10) - 2] +
+            "-" +
+            ones[Math.floor(min % 10)]
+        );
+    }
+}
+
+function updateTime() {
+    var date = new Date();
+    var hours = ("0" + date.getHours()).slice(-2);
+    var minutes = ("0" + date.getMinutes()).slice(-2);
+    var seconds = ("0" + date.getSeconds()).slice(-2);
+    var currentTime = `${hours}:${minutes}:${seconds}`;
+    timeHeader.textContent = currentTime;
+    timeWords.textContent =
+        "IT IS " +
+        hourToWord(hours).toUpperCase() +
+        minutesToWords(minutes).toUpperCase();
+    setTimeout(updateTime, 1000);
+}
+
+updateTime();
+
+const quoteSpan = document.querySelector("#quote");
+const authorSpan = document.querySelector("#author");
+const quoteButton = document.querySelector(".quoteButton");
+
+const getNewQuote = async () => {
+    var url = "https://favqs.com/api/qotd";
+    const response = await fetch(url);
+    const gotQuote = await response.json();
+    if (gotQuote) {
+        quoteSpan.textContent = gotQuote.quote.body;
+        authorSpan.textContent = '- ' + gotQuote.quote.author;
+    }
+};
+
+quoteButton.addEventListener("click", function () {
+    getNewQuote();
+});
+
+getNewQuote();
+
+const colorChangeButton = document.getElementById('colorChangeButton');
+const color = document.querySelector('.colorHex');
+const colorDiv = document.querySelector('.colorGenerator');
+
+colorChangeButton.addEventListener('click', function() {
+    const generatedColor = '#' + [...Array(6)].map(() => Math.floor(Math.random() * 16).toString(16)).join('').toUpperCase();
+    colorDiv.style.backgroundColor = generatedColor;
+    color.textContent = generatedColor;
+});
+
+
+const stopwatchButton = document.querySelectorAll(".btn");
+const stopwatch = document.getElementById("timer");
+
+var stop = false;
+
+let ms = 0;
+let sec = 0;
+let min = 0;
+
+function startStop() {
+    if (stop) {
+        console.log(performance.memory);
+        return;
+    }
+    ms++;
+    if (ms == 60) {
+        ms = 0;
+        sec++;
+        if (sec == 60) {
+            sec = 0;
+            min++;
+        }
+    }
+    displayTimer(min, sec, ms);
+    setTimeout(startStop, 10);
+}
+
+function resetTimer() {
+    ms = 0;
+    sec = 0;
+    min = 0;
+    displayTimer(min, sec, ms);
+}
+
+function displayTimer(min, sec, ms) {
+    min = ("0" + min).slice(-2);
+    sec = ("0" + sec).slice(-2);
+    ms = ("0" + ms).slice(-2);
+    stopwatch.textContent = `${min}:${sec}:${ms}`;
+}
+
+stopwatchButton.forEach(function (button) {
+    button.addEventListener("click", function () {
+        if (button.id == "start") {
+            stop = false;
+            startStop();
+        }
+        if (button.id == "stop") {
+            stop = true;
+        }
+        if (button.id == "reset") {
+            resetTimer();
+        }
+    });
+});
+
+
+let counter = 0;
+let increment = 1;
+
+const counterVal = document.querySelector(".counterValue");
+const ctrButtons = document.querySelectorAll(".ctrButton");
+const variation = document.querySelector("#variation");
+
+variation.addEventListener("input", function () {
+  increment = Number(variation.value);
+});
+
+ctrButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    if (button.id == "increase") {
+      counter += increment;
+    } else if (button.id == "decrease") {
+      counter -= increment;
+    } else {
+      counter = 0;
+    }
+
+    if (counter > 0) {
+      counterVal.style.color = "green";
+    } else if (counter < 0) {
+      counterVal.style.color = "red";
+    } else {
+      counterVal.style.color = "black";
+    }
+
+    counterVal.textContent = counter;
+  });
+});
+
+
 // selectors
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
@@ -26,13 +224,13 @@ function addTodo(event) {
 
         // Mark finished items
         const completedButton = document.createElement('button');
-        completedButton.innerHTML = `<i class="bi bi-check2-square"></i>`
+        completedButton.textContent = `Done`;
         completedButton.classList.add("complete-btn")
         todoDiv.appendChild(completedButton);
 
         // Delete item
         const deleteButton = document.createElement('button');
-        deleteButton.innerHTML = `<i class="bi bi-trash-fill"></i>`
+        deleteButton.textContent = `Delete`;
         deleteButton.classList.add("trash-btn")
         todoDiv.appendChild(deleteButton);
 
@@ -66,3 +264,4 @@ function checkDelete(e) {
     }
 
 }
+
